3f6b0b0985a174895fa3208d81fceb6a3a5062f4
razan-am <razanalamleh@gmail.com>
2021-09-21 23:37:16 +0300

    lab 33

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import ToDo from './components/todo/todo.js';
 import SettingProvider from './context/context .js';
+import 'normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 export default class App extends React.Component {
   render() {
